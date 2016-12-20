32334d6f797d68665237c5e5134af64da31160fd
pengpeng12 <18236933571@139.com>
2016-12-20 13:08:20 +0800

    kvc-运算

--- KVCAndKVODemo/ViewController.m
+++ KVCAndKVODemo/ViewController.m
@@ -36,15 +36,46 @@
     
     
     //------------KVC键值路径
-    Book *book = [[Book alloc] init];
+//    Book *book = [[Book alloc] init];
+//    Author *author = [[Author alloc] init];
+//    //设置作者
+//    [book setValue:author forKey:@"author"];
+//    //设置作者的名字
+//    //路径为：author.name,中间用点号进行连接
+//    [book setValue:@"jiangwei" forKeyPath:@"author.name"];
+//    NSString *authorName = [author valueForKey:@"name"];
+//    NSLog(@"authorName is--%@",authorName);
+    
+    //-------------KVC的运算
     Author *author = [[Author alloc] init];
-    //设置作者
-    [book setValue:author forKey:@"author"];
-    //设置作者的名字
-    //路径为：author.name,中间用点号进行连接
-    [book setValue:@"jiangwei" forKeyPath:@"author.name"];
-    NSString *authorName = [author valueForKey:@"name"];
-    NSLog(@"authorName is--%@",authorName);
+    [author setValue:@"莫言" forKeyPath:@"name"];
+    
+    Book *book1 = [[Book alloc] init];
+    book1.name = @"红高粱";
+    book1.price = 9;
+    Book *book2 = [[Book alloc] init];
+    book2.name = @"蛙";
+    book2.price = 10;
+    NSArray *array = [NSArray arrayWithObjects:book1,book2, nil];
+    [author setValue:array forKeyPath:@"issueBook"];
+    
+    //基本数据类型会自动被包装成NSNumber，装到数组中
+    //得到所有书籍的价格
+    NSArray *priceArray = [author valueForKeyPath:@"issueBook.price"];
+    NSLog(@"priceArray---%@",priceArray);
+    //获取数组的大小
+    NSNumber *count = [author valueForKeyPath:@"issueBook.@count"];
+    NSLog(@"count = %@",count);
+    //获取书籍价格的总和
+    NSNumber *sum = [author valueForKeyPath:@"issueBook.@sum.price"];
+    NSLog(@"priceSum= %@",sum);
+    //获取书籍的平均值
+    NSNumber *avg = [author valueForKeyPath:@"issueBook.@avg.price"];
+    NSLog(@"avg-%@",avg);
+    //获取书籍的价格最大值最小值
+    NSNumber *max = [author valueForKeyPath:@"issueBook.@max.price"];
+    NSNumber *min = [author valueForKeyPath:@"issueBook.@min.price"];
+    
     
     
     
